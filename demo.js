e000100bbcbfef3bb5aa0c3a832490cdf73d9654
Stuart Nelson <stuartnelson3@gmail.com>
2013-09-17 10:08:03 -0400

    Data is now a set of planets with pseudo-accurate sizes
    Updated js accordingly to deal with an array of objects, instead of an
    array of ints.

--- demo.js
+++ demo.js
@@ -8,15 +8,15 @@ document.addEventListener('DOMContentLoaded', function() {
     circles.enter().append('circle')
     .transition()
     .duration(750)
-    .attr('cx', 0)
+    .attr('cy', 0)
+    .attr('cx', function(d,i) { return i*70 + 60; })
     .attr('r', 0)
 
     circles.transition()
     .duration(2500)
     .delay(function(d,i) { return i*250; })
     .attr('cy', 200)
-    .attr('cx', function(d,i) { return d*60; })
-    .attr('r', function(d,i) { return d*3; })
+    .attr('r', function(d,i) { return d.size*3; })
 
     svg.selectAll('circle')
     .data(data)
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', function() {
     .duration(2500)
     .delay(function(d,i) { return i*250; })
     .attr('cy', 2800)
-    .attr('cx', function(d) { return d*100; })
+    .attr('cx', function(d) { return 0; })
     .attr('r', 0)
     .remove()
 
@@ -36,31 +36,40 @@ document.addEventListener('DOMContentLoaded', function() {
     var text = svg.selectAll('text').data(data);
 
     text.enter().append('text')
-    .attr('y', function(d,i) { return 0; })
-    .attr('x', function(d,i) { return d*60 + 13; })
-    .attr('dx', -3) // padding-right
+    .attr('y', function(d,i) { return 500; })
+    .attr('x', function(d,i) { return i*70 + 60; })
     .attr('dy', '.35em') // vertical-align: middle
-    .attr('text-anchor', 'end') // text-align: right
+    .attr('text-anchor', 'middle') // text-align: middle
     .text(function(d) { return d.name; });
 
     text.transition().duration(2500)
     .delay(function(d,i) { return i*250; })
-    .attr('y', function(d) {return 210 + d*3;})
+    .attr('y', function(d) {return 210 + d.size*3;})
     .text(function(d) { return d.name || 'dot'; });
+
+    svg.selectAll('text')
+    .data(data)
+    .exit()
+    .transition()
+    .duration(2500)
+    .delay(function(d,i) { return i*250; })
+    .attr('y', 2800)
+    .attr('x', function(d) { return 0; })
+    .remove()
   }
 
-  data = [1,2,3,4,5,6,7,8,9];
-  // data = [
-  //   {name: 'Mercury', size: 2},
-  //   {name: 'Venus', size: 3},
-  //   {name: 'Earth', size: 3.5},
-  //   {name: 'Mars', size: 2.5},
-  //   {name: 'Jupiter', size: 10},
-  //   {name: 'Saturan', size: 7},
-  //   {name: 'Uranus', size: 5.5},
-  //   {name: 'Neptune', size: 4},
-  //   {name: 'Pluto', size: 1},
-  // ];
+  // data = [1,2,3,4,5,6,7,8,9];
+  data = [
+    {name: 'Mercury', size: 2},
+    {name: 'Venus', size: 3},
+    {name: 'Earth', size: 3.5},
+    {name: 'Mars', size: 2.5},
+    {name: 'Jupiter', size: 10},
+    {name: 'Saturn', size: 7},
+    {name: 'Uranus', size: 5.5},
+    {name: 'Neptune', size: 4},
+    {name: 'Pluto', size: 1},
+  ];
   addCircles(data);
 
 })
